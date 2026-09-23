22cb4fa1a1e0a006cfd31d89a8aff154b6217833
AshokaTano1977 <cconforti@gmail.com>
2026-09-23 14:54:56 -0300

    listo

--- scripts.js
+++ scripts.js
@@ -42,4 +42,4 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".cupos-numero").forEach((span) => {
       if (span.textContent === "...") span.textContent = "—";
     });
-  });
+  }); })
